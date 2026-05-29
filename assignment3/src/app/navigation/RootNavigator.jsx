f7befab9033c685eaff212ef4fa550b7c07bc4ee
Clayton Ma <clayton.mkc@gmail.com>
2026-05-30 02:35:11 +1000

    deep linking

--- assignment3/src/app/navigation/RootNavigator.jsx
+++ assignment3/src/app/navigation/RootNavigator.jsx
@@ -6,55 +6,55 @@ import AuthNavigator from "./AuthNavigator";
 import TabNavigator from "./TabNavigator";
 
 const prefix = Linking.createURL("/");
-
-const linking = {
-  prefixes: [prefix],
-  config: {
-    screens: {
-      // Auth routes
-      Login: "login",
-      Register: "register",
-      About: "about",
-
-      // App routes (when authenticated)
-      // The structure matches TabNavigator
-      MainTabs: {
-        screens: {
-          Quiz: {
-            screens: {
-              QuizList: "quiz",
-              EditQuiz: "quiz/edit/:id",
-              CreateQuiz: "quiz/create",
-            },
-          },
-          Group: {
-            screens: {
-              GroupList: "group",
-              EditGroup: "group/edit/:id",
-            },
-          },
-          User: {
-            screens: {
-              UserSettingList: "user/settings",
-              UserProfile: "user/profile",
-              About: "user/about",
-            },
-          },
-        },
-      },
-      Attempt: {
-        screens: {
-          ListAttempt: "attempt",
-          TakeAttempt: "attempt/take/:id",
-          ViewAttempt: "attempt/view/:id",
-        },
-      },
-    },
-  },
-};
+const universal = "https://barossa02.ifn666.com"; // Handles your web domain
 
 export function RootNavigator() {
   const { user } = useAuth();
+  const linking = {
+    prefixes: [prefix, universal],
+    config: {
+      screens: {
+        // Auth routes
+        Login: "login",
+        Register: "register",
+        About: "about",
+
+        // App routes (when authenticated)
+        // The structure matches TabNavigator
+        MainTabs: {
+          screens: {
+            Quiz: {
+              screens: {
+                QuizList: "quiz",
+                EditQuiz: "quiz/edit/:id",
+                CreateQuiz: "quiz/create",
+              },
+            },
+            Group: {
+              screens: {
+                GroupList: "group",
+                EditGroup: "group/edit/:id",
+              },
+            },
+            User: {
+              screens: {
+                UserSettingList: "user/settings",
+                UserProfile: "user/profile",
+                About: "user/about",
+              },
+            },
+          },
+        },
+        Attempt: {
+          screens: {
+            ListAttempt: "attempt",
+            TakeAttempt: "attempt/take/:id",
+            ViewAttempt: "attempt/view/:id",
+          },
+        },
+      },
+    },
+  };
   return (
     <NavigationContainer linking={linking}>
       {user ? <TabNavigator /> : <AuthNavigator />}
